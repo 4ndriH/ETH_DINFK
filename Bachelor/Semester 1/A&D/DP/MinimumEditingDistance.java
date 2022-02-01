5ff9f8c40c82ec392f95160549a402ec61b24e38
4ndriH <45722342+4ndriH@users.noreply.github.com>
2022-02-01 18:33:29 +0100

    Update MinimumEditingDistance.java

--- Bachelor/Semester 1/A&D/DP/MinimumEditingDistance.java
+++ Bachelor/Semester 1/A&D/DP/MinimumEditingDistance.java
@@ -7,16 +7,21 @@ public class MinimumEditingDistance {
     }
 
     static int dp(String str1, String str2) {
-        int[][] dp = new int[str1.length() + 1][str2.length() + 1];
+       int[][] dp = new int[str1.length() + 1][str2.length() + 1];
 
-        for (int i = 1; i <= str1.length(); i++) {
-            for (int j = 1; j <= str2.length(); j++) {
-                int d = str1.charAt(i - 1) == str2.charAt(j - 1) ? 0 : 1;
-                dp[i][j] = min(dp[i][j - 1] + d, min(dp[i - 1][j] + d, dp[i - 1][j - 1] + d));
-            }
-        }
+		for (int i = 0; i <= str1.length(); i++) {
+			for (int j = 0; j <= str2.length(); j++) {
+				if (i == 0) {
+					dp[i][j] = j;
+				} else if (j == 0) {
+					dp[i][j] = i;
+				} else {
+					dp[i][j] = min(dp[i - 1][j - 1] + (str1.charAt(i - 1) == str2.charAt(j - 1) ? 0 : 1), min(dp[i - 1][j] + 1, dp[i][j - 1] + 1));
+				}
+			}
+		}
 
-        return dp[str1.length()][str2.length()];
+		return dp[str1.length()][str2.length()];
     }
 
     static int min(int a, int b) {
