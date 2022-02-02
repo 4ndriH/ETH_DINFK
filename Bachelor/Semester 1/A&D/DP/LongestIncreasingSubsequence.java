33be0a532d3a612113c7b62fd4caead01fd5d783
4ndriH <45722342+4ndriH@users.noreply.github.com>
2022-02-02 11:23:43 +0100

    Update LongestIncreasingSubsequence.java

--- Bachelor/Semester 1/A&D/DP/LongestIncreasingSubsequence.java
+++ Bachelor/Semester 1/A&D/DP/LongestIncreasingSubsequence.java
@@ -4,34 +4,38 @@ public class LongestIncreasingSubsequence {
 
         System.out.println(dp(a));
     }
-
+    
+    // Code from HumbleCatcher
     static int dp(int[] a) {
-        int[] dp = new int[a.length];
-        int length = 1;
-    	
-        for (int i = 1; i < a.length; i++) {
-            if (a[i] < dp[0]) {
-            	dp[0] = a[i];
-            } else if (a[i] > dp[length - 1]) {
-            	dp[length++] = a[i];
-            } else {
-            	dp[BinarySearch(dp, -1, length - 1, a[i])] = a[i];
-            }
+        int n = a.length;
+        int[] dp = new int[n];
+        int length = 0;
+
+        for (int i = 0; i < n; i++) {
+            dp[i] = Integer.MAX_VALUE;
         }
+
+        for (int i = 0; i < n; i++) {
+		    int j = binarySearch(dp, a[i], 0, length);	
+            dp[j] = a[i];
+            if (j == length) {
+                length++;
+            }
+		}
         
         return length;
     }
 
-     static int BinarySearch(int[] a, int left, int right, int search) {
-         while (right - left > 1) {
-             int mid = left + (right - left) / 2;
-             if (a[mid] < search) {
-            	 left = mid;
-             } else {
-            	 right = mid;
-             }
-         }
-  
-         return right;
-     }
+     static int binarySearch(int[] dp, int x, int l, int r) {
+        while (r > l) {
+            int m = (r + l) / 2;
+            if (dp[m] < x) {
+                l = m + 1;
+            } else {
+                r = m;
+            }
+        }
+
+        return l;
+    }
 }
